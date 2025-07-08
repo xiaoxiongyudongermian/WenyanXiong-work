ae5e3805f5bbb6ba4d9fa8ce63d156eeb82798f3
Wenyan Xiong <2119548948@qq.com>
2025-07-08 15:36:23 +0800

    SM4软件实现及其优化

--- Project1/SM4/function.cpp
+++ Project1/SM4/function.cpp
@@ -1,6 +1,8 @@
 #include <cstdint>
 
 extern const uint8_t Sbox[16][16];
+// 利用查表优化
+uint32_t T_table0[256], T_table1[256], T_table2[256], T_table3[256];
 
 uint32_t Rotate_Left(uint32_t x, int d)
 {
@@ -8,7 +10,14 @@ uint32_t Rotate_Left(uint32_t x, int d)
 	return (x << (d % 32)) | (x >> (32 - d % 32));
 }
 
+uint32_t L(uint32_t B)
+{
+	// C = L(B) = B ^ (B <<< 2) ^ (B <<< 10) ^ (B <<< 18) ^ (B <<< 24)
+	uint32_t C = B ^ (Rotate_Left(B, 2)) ^ (Rotate_Left(B, 10)) ^ (Rotate_Left(B, 18)) ^ (Rotate_Left(B, 24));
+	return C;
+}
 
+// 实现T函数
 uint32_t T(uint32_t A)
 {
 	// 合成置换T函数，T(A) = L(r(A))
@@ -24,8 +33,7 @@ uint32_t T(uint32_t A)
 	}
 	uint32_t B = (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3];
 	// C = L(B) = B ^ (B <<< 2) ^ (B <<< 10) ^ (B <<< 18) ^ (B <<< 24)
-	uint32_t C = B ^ (Rotate_Left(B, 2)) ^ (Rotate_Left(B, 10)) ^ (Rotate_Left(B, 18)) ^ (Rotate_Left(B, 24));
-	return C;
+	return L(B);
 }
 
 uint32_t F(uint32_t X0, uint32_t X1, uint32_t X2, uint32_t X3, uint32_t rk)
@@ -34,5 +42,30 @@ uint32_t F(uint32_t X0, uint32_t X1, uint32_t X2, uint32_t X3, uint32_t rk)
 	return X0 ^ T(X1 ^ X2 ^ X3 ^ rk);
 }
 
+
+
+void init_T_table()
+{
+	for (int i = 0; i < 256; i++)
+	{
+		uint8_t b = Sbox[i / 16][i % 16];
+		T_table0[i] = L(b << 24);
+		T_table1[i] = L(b << 16);
+		T_table2[i] = L(b << 8);
+		T_table3[i] = L(b);
+	}
+	
+}
+//优化后轮函数
+uint32_t OptimizedF(uint32_t X0, uint32_t X1, uint32_t X2, uint32_t X3, uint32_t rk)
+{
+	// 轮函数F，F(X0,X1,X2,X3,rk) = X0 ^ T_table(X1 ^ X2 ^ X3 ^ rk)
+	uint32_t A = X1 ^ X2 ^ X3 ^ rk;
+
+	return X0 ^ T_table0[(A >> 24) % 256] ^ T_table1[(A >> 16) % 256] ^ T_table2[(A >> 8) % 256] ^ T_table3[A % 256];
+}
+
 extern uint32_t F(uint32_t , uint32_t , uint32_t , uint32_t , uint32_t );
 extern uint32_t Rotate_Left(uint32_t , int );
+extern uint32_t OptimizedF(uint32_t X0, uint32_t X1, uint32_t X2, uint32_t X3, uint32_t rk);
+extern void init_T_table();
--- Project1/SM4/test_main.cpp
+++ Project1/SM4/test_main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <chrono>
 
 using namespace std;
  
 extern uint32_t* encrypt(uint32_t* X, uint32_t* MK);
 extern uint32_t* decrypt(uint32_t* Y, uint32_t* MK);
+
+extern uint32_t* Optimized_encrypt(uint32_t* X, uint32_t* MK);
+extern uint32_t* Optimized_decrypt(uint32_t* X, uint32_t* MK);
+
+extern void init_T_table();
+
 int main()
 {
+	init_T_table();
+
 	uint32_t* X = new uint32_t[4];
 	uint32_t* MK = new uint32_t[4];
 	X[0] = 0x01234567;
@@ -34,13 +43,31 @@ int main()
 	for (int i = 0; i < 4; i++)cout << hex << Xtemp[i] ;
 	cout << endl;
 
-
+	//测试时间
+	for (int i = 0; i < 4; i++)Xtemp[i] = X[i];
+	//优化前
+	auto t1 = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < 1000000; i++)
 	{
 		X = encrypt(X, MK);
 	}
+	auto t2 = std::chrono::high_resolution_clock::now();
+	cout << "优化前进行1000000次加密后结果：";
 	for (int i = 0; i < 4; i++)cout << hex << X[i];
-	cout << endl;
+	cout << "，耗时:" << dec << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " 微秒" << endl;
+
+
+	//优化后
+	auto t3 = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 1000000; i++)
+	{
+		Xtemp = Optimized_encrypt(Xtemp, MK);
+	}
+	auto t4 = std::chrono::high_resolution_clock::now();
+	cout << "优化后进行1000000次加密后结果：";
+	for (int i = 0; i < 4; i++)cout << hex << Xtemp[i];
+	cout << "，耗时:" << dec << std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count() << " 微秒" << endl;
+
 
 	delete[] X;
 	delete[] MK;
